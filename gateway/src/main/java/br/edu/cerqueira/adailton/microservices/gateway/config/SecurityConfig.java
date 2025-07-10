c0ad1db96e6f831173fb01b389cedfecb77b5582
Adailton de Jesus Cerqueira Junior <adailton.junior@gmail.com>
2025-07-10 14:13:14 -0300

    feat: Configurando security no gateway. Modificando o arquivo application.properties para yml.

--- gateway/src/main/java/br/edu/cerqueira/adailton/microservices/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/br/edu/cerqueira/adailton/microservices/gateway/config/SecurityConfig.java
@@ -1,37 +1,23 @@
 package br.edu.cerqueira.adailton.microservices.gateway.config;
 
-import br.edu.cerqueira.adailton.microservices.gateway.converter.JwtConverter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebFluxSecurity
 public class SecurityConfig {
-    @Autowired
-    private JwtConverter jwtConverter;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(AbstractHttpConfigurer::disable)
-            .authorizeHttpRequests((authorize)-> authorize
-                    .requestMatchers("/api/v1").permitAll()
-                    .requestMatchers("api/v1/admin").hasRole("admin")
-                    .anyRequest().authenticated())
-            .oauth2ResourceServer(
-                (oauth2)-> oauth2.jwt(
-                        jwt-> jwt.jwtAuthenticationConverter(jwtConverter)
-                ))
-            .sessionManagement(
-                session-> session.sessionCreationPolicy(
-                    SessionCreationPolicy.STATELESS)
-            );
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) throws Exception {
+        http.authorizeExchange(auth -> auth.anyExchange().authenticated())
+                .oauth2Login(withDefaults())
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(withDefaults()));
+        http.csrf(ServerHttpSecurity.CsrfSpec::disable);
 
         return http.build();
     }
---
+++ gateway/src/main/java/br/edu/cerqueira/adailton/microservices/gateway/controller/TokenController.java
@@ -0,0 +1,16 @@
+package br.edu.cerqueira.adailton.microservices.gateway.controller;
+
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class TokenController {
+
+    @GetMapping(value = "/token")
+    public Mono<String> getHome(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient) {
+        return Mono.just(authorizedClient.getAccessToken().getTokenValue());
+    }
+}
