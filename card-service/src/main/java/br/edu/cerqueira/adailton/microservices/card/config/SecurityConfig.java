b391ce20589df7df39b3512f094aea735785ca55
Adailton de Jesus Cerqueira Junior <adailton.junior@gmail.com>
2025-07-13 18:20:25 -0300

    feat: Adicionando a configuração para rotas publicas.

--- card-service/src/main/java/br/edu/cerqueira/adailton/microservices/card/config/SecurityConfig.java
+++ card-service/src/main/java/br/edu/cerqueira/adailton/microservices/card/config/SecurityConfig.java
@@ -10,12 +10,19 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+
+    private static final String[] PUBLIC_LIST = {
+            "/category", "/swagger-resources", "/swagger-resources/**",
+    };
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
-                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(PUBLIC_LIST).permitAll()
+                .anyRequest().authenticated())
+            .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
         return http.build();
     }
 }
