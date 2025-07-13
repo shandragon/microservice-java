c823805ce80cb77b6d4626c88759d8aacc7e6933
Adailton de Jesus Cerqueira Junior <adailton.junior@gmail.com>
2025-07-13 12:28:13 -0300

    feat(keycloak): Finalizando a configuração da autenticação no keycloak.

--- card-service/src/main/java/br/edu/cerqueira/adailton/microservices/card/config/SecurityConfig.java
+++ card-service/src/main/java/br/edu/cerqueira/adailton/microservices/card/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package br.edu.cerqueira.adailton.microservices.card.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
